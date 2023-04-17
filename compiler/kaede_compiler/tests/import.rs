67027f5c784a73894136c92e854dbd77f5d9b12d
yohashinoio <shinodeveloper1@gmail.com>
2023-04-17 13:26:27 +0900

    Refactor

--- compiler/kaede_compiler/tests/import.rs
+++ compiler/kaede_compiler/tests/import.rs
@@ -15,24 +15,24 @@ fn create_kaede_file_name(stem: &str) -> String {
 fn import_functions() -> anyhow::Result<()> {
     let tmp_dir = assert_fs::TempDir::new()?;
 
-    let m1 = tmp_dir.child(create_kaede_file_name("m1"));
-    m1.write_str("pub fn yoha() -> i32 { return 48 }")?;
+    let mod_1 = tmp_dir.child(create_kaede_file_name("m1"));
+    mod_1.write_str("pub fn yoha() -> i32 { return 48 }")?;
 
-    let m2 = tmp_dir.child(create_kaede_file_name("m2"));
-    m2.write_str("pub fn io() -> i32 { return 10 }")?;
+    let mod_2 = tmp_dir.child(create_kaede_file_name("m2"));
+    mod_2.write_str("pub fn io() -> i32 { return 10 }")?;
 
     let main = tmp_dir.child(create_kaede_file_name("main"));
     main.write_str("import m1\nimport m2\nfn main() -> i32 { return m1.yoha() + m2.io() }")?;
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.args([main.path(), m1.path(), m2.path()]);
+    cmd.args([main.path(), mod_1.path(), mod_2.path()]);
     let success = cmd.assert().success();
 
-    let llvm_ir = assert_fs::NamedTempFile::new("ir")?;
-    llvm_ir.write_binary(&success.get_output().stdout)?;
+    let ir = assert_fs::NamedTempFile::new("ir")?;
+    ir.write_binary(&success.get_output().stdout)?;
 
     Command::new("lli")
-        .arg(llvm_ir.path())
+        .arg(ir.path())
         .assert()
         .code(predicate::eq(58));
 
