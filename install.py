09ebc7eb61548e6133b31aa95440932ba8ae5603
yohashinoio <shinodeveloper1@gmail.com>
2024-01-29 15:33:59 +0900

    Fix the install script
    By expanding environment variables that were trying to open files without expanding them.
    Change mode from "x" to "w+" when opening the env file.

--- install.py
+++ install.py
@@ -6,8 +6,10 @@ import subprocess
 import shutil
 
 
-kaede_dir = os.path.expanduser("~/.kaede")
-kaede_bin_dir = os.path.join(kaede_dir, "bin")
+unexpanded_kaede_dir = "$HOME/.kaede"
+kaede_dir = os.path.expandvars(unexpanded_kaede_dir)
+unexpanded_kaede_bin_dir = "$HOME/.kaede/bin"
+kaede_bin_dir = os.path.expandvars(unexpanded_kaede_bin_dir)
 
 
 def install_kaede():
@@ -31,17 +33,18 @@ if __name__ == '__main__':
     if not os.path.exists(kaede_bin_dir):
         os.mkdir(kaede_bin_dir)
 
+    # Install
     library.install(kaede_dir)
-
     os.environ["KAEDE_DIR"] = kaede_dir
     install_kaede()
 
     # Create shell script for setting environment variables
-    env_script_path = "$HOME/.kaede/env"
-    with open(env_script_path, "x") as f:
-        f.write("export PATH=$PATH:%s" % kaede_bin_dir + "\n")
+    unexpanded_env_script_path = os.path.join(unexpanded_kaede_dir, "env")
+    with open(os.path.expandvars(unexpanded_env_script_path), "w+") as f:
+        f.writelines(["#!/bin/sh\n", "\n", 'export PATH="%s:$PATH"\n' %
+                     unexpanded_kaede_bin_dir])
     with open(os.path.expanduser("~/.profile"), "a+") as f:
-        f.write('. "%s"' % env_script_path + "\n")
+        f.write('. "%s"' % unexpanded_env_script_path + "\n")
 
     print("Enter the following commands:")
     print("source ~/.profile")
