0f8cdf4a52f57b8168f1373fbd1c093e014d9850
yohashinoio <shinodeveloper1@gmail.com>
2024-07-25 21:44:29 +0900

    Add a test for importing struct methods

--- compiler/driver/tests/import.rs
+++ compiler/driver/tests/import.rs
@@ -55,7 +55,7 @@ fn import_i32_methods() -> anyhow::Result<()> {
             pub fn add(self, other: i32): i32 {
                 return self + other
             }
-    }"#,
+        }"#,
     )?;
 
     let main = tempdir.child("main.kd");
@@ -71,6 +71,74 @@ fn import_i32_methods() -> anyhow::Result<()> {
             "--display-llvm-ir",
             main.path().to_str().unwrap(),
             module.path().to_str().unwrap(),
+        ])
+        .assert()
+        .success();
+
+    let llvm_ir = assert_fs::NamedTempFile::new("ir")?;
+    llvm_ir.write_binary(&compile_output.get_output().stdout)?;
+
+    Command::new("lli")
+        .arg(llvm_ir.path())
+        .assert()
+        .code(predicate::eq(58));
+
+    Ok(())
+}
+
+#[test]
+fn import_struct_methods() -> anyhow::Result<()> {
+    let tempdir = assert_fs::TempDir::new()?;
+
+    let module1 = tempdir.child("m1.kd");
+    module1.write_str(
+        r#"struct Apple {
+            size: i32,
+        }
+        impl Apple {
+            pub fn new(size: i32): Apple {
+                return Apple { size: size }
+            }
+
+            pub fn is_orange(self): bool {
+                return false
+            }
+        }"#,
+    )?;
+
+    let module2 = tempdir.child("m2.kd");
+    module2.write_str(
+        r#"struct Ichigo {
+            size: i32,
+        }
+        impl Ichigo {
+            pub fn get_size(self): i32 {
+                return self.size
+            }
+        }
+        "#,
+    )?;
+
+    let main = tempdir.child("main.kd");
+    main.write_str(
+        r#"import m1
+        import m2
+        fn main(): i32 {
+            let apple = m1.Apple::new(48);
+            let ichigo = m2.Ichigo { size: 10 }
+            if !apple.is_orange() {
+                return apple.size + ichigo.get_size()
+            }
+            return 123
+        }"#,
+    )?;
+
+    let compile_output = Command::cargo_bin(env!("CARGO_BIN_EXE_kaede"))?
+        .args([
+            "--display-llvm-ir",
+            main.path().to_str().unwrap(),
+            module1.path().to_str().unwrap(),
+            module2.path().to_str().unwrap(),
         ])
         .assert()
         .success();
